f151bdb2f9cc20a1ac4edfab84c29fcda93dd2c3
AltoSax <AltoSax00@bk.ru>
2016-08-05 01:33:17 +0400

    Fist load to the Git

---
+++ src/main/java/programm/Programm.java
@@ -0,0 +1,13 @@
+package programm;
+
+/**
+ * Created by Savva Avdysho on 05.08.2016.
+ */
+
+public class Programm {
+    public static void main(String[] args) {
+        System.out.println("Ok, what I need to do?");
+        System.out.println("Print smth, load it to GitHub and update...");
+        System.out.println("Sounds easy");
+    }
+}
